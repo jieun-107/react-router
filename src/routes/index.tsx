d3674e1ce9476d05a673e5704e4a4e2df434fb7d
jelee <lje7128@gmail.com>
2026-03-05 16:12:19 +0900

    레이아웃 라우트

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,37 +4,47 @@ import About from "../pages/About";
 import DashboardHome from "../pages/dashboard/DashboardHome";
 import Dashboard from "../pages/dashboard/Dashboard";
 import DashboardSetting from "../pages/dashboard/DashboardSetting";
+import Default from "../pages/layouts/Default";
+import DashboardLayout from "../pages/layouts/DashboardLayout";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    Component: Home,
-  }, // 라우트 객체
-  {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "/post",
-    element: <h1>Post page</h1>,
-  },
-  {
-    path: "/dashboard",
-    Component: Dashboard,
+    Component: Default,
     children: [
       {
-        // path: "", // /dashboard
-        index: true, // 인덱스 라우트
-        Component: DashboardHome,
+        path: "/",
+        Component: Home,
+      }, // 라우트 객체
+      {
+        path: "/about",
+        element: <About />,
       },
       {
-        path: "setting", // /dashboard/setting (children 속성으로 중첩된 path에는 /를 적지 않음.)
-        Component: DashboardSetting,
-        children: [{
-          index: true,
-          element: <h1>DashboardSetting Home</h1>
-        }]
-      }
+        Component: DashboardLayout,
+        children: [
+          {
+            path: "/dashboard",
+            Component: Dashboard,
+            children: [
+              {
+                // path: "", // /dashboard
+                index: true, // 인덱스 라우트
+                Component: DashboardHome,
+              },
+              {
+                path: "setting", // /dashboard/setting (children 속성으로 중첩된 path에는 /를 적지 않음.)
+                Component: DashboardSetting,
+                children: [
+                  {
+                    index: true,
+                    element: <h1>DashboardSetting Home</h1>,
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
     ],
   },
 ]);
